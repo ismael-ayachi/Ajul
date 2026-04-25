b8ac70af5325d80329932347cb051703f230f38e
ismaelayachi <ismael.ayachi@epfl.ch>
2026-04-25 16:19:13 +0200

    Étape 9 V1.2

--- src/ch/epfl/ajul/gui/BoardUI.java
+++ src/ch/epfl/ajul/gui/BoardUI.java
@@ -2,6 +2,7 @@ package ch.epfl.ajul.gui;
 
 import ch.epfl.ajul.*;
 import ch.epfl.ajul.gamestate.ImmutableGameState;
+import ch.epfl.ajul.gamestate.packed.PkWall;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
@@ -12,6 +13,7 @@ import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -41,9 +43,6 @@ public final class BoardUI {
         sourceGrid.setId("tile-sources");
         root.getChildren().add(sourceGrid);
 
-        GridPane playerBoardGrid = new GridPane();
-        playerBoardGrid.setId("player-boards");
-        root.getChildren().add(playerBoardGrid);
 
         //Fabriques
         for (TileSource factory : observer.getValue().game().factories()) {
@@ -72,20 +71,78 @@ public final class BoardUI {
 
         //Plateaux des joueurs
 
+        GridPane playerBoardGrid = new GridPane();
+        playerBoardGrid.setId("player-boards");
+        root.getChildren().add(playerBoardGrid);
 
+        Map<PlayerId, StackPane> playerBoards = new HashMap<>();
 
         for (PlayerId playerId: observer.getValue().playerIds()){
+            //Plateau du joueur courant
             StackPane currentPlayerBoard = new StackPane();
             currentPlayerBoard.getStyleClass().addAll("player-board");
 
+            playerBoards.put(playerId, currentPlayerBoard);
+            //Contenu du plateau du joueur courant
             VBox gridContent = new VBox();
-
             currentPlayerBoard.getChildren().add(gridContent);
 
+
+            //Lignes de motif et mur
+
+            GridPane patternWall = new GridPane();
+            patternWall.getStyleClass().add("lines-and-wall");
+
+            gridContent.getChildren().add(patternWall);
+
+            for (int row = 0; row < PkWall.WALL_WIDTH; row++){
+
+                HBox patternBox = new HBox();
+
+                patternBox.getStyleClass().addAll("tile-destination" , "tile-group");
+                for (int count = 0; count < row + 1; count++) {
+                    Node anchor =
+                            anchors.get(new TileLocation.OnPattern(playerId,TileDestination.Pattern.ALL.get(row), count));
+                    GridPane.setHalignment(patternBox, HPos.RIGHT);
+                    GridPane.setFillWidth(patternBox, false);
+                    patternBox.getChildren().add(anchor);
+
+                }
+                patternWall.add(patternBox, 0, row);
+
+                Text fullColorBonus = new Text("+10");
+                patternWall.add(fullColorBonus, 1, row);
+
+
+                for (int col = 2; col < 7 ; col++) {
+
+                    Node anchor = anchors.get(
+                            new TileLocation.OnWall(
+                                    playerId,
+                                    TileDestination.Pattern.ALL.get(row),
+                                    PkWall.colorAt(TileDestination.Pattern.ALL.get(row), col-2)));
+                    anchor.getStyleClass().addAll("wall-background",
+                            PkWall.colorAt(TileDestination.Pattern.ALL.get(row), col-2).toString());
+
+                    patternWall.add(anchor, col, row);
+
+                }
+
+                Text fullRowBonus = new Text("+2");
+                patternWall.add(fullRowBonus, 7, row);
+            }
+
+            for (int col = 2; col < 7 ; col++) {
+                Text fullColBonus = new Text("+7");
+                GridPane.setHalignment(fullColBonus, HPos.CENTER);
+                patternWall.add(fullColBonus, col, 5);
+            }
+
+            //Ligne plancher
             HBox floor = new HBox();
             floor.getStyleClass().addAll("floor", "tile-group", "tile-destination");
-
             gridContent.getChildren().add(floor);
+
 
             for (int i = 0; i < TileDestination.FLOOR.capacity(); i++){
                 VBox floorPositions = new VBox();
@@ -95,16 +152,20 @@ public final class BoardUI {
 
                 Text penalty = new Text(Integer.toString(Points.floorPenalty(i)));
                 floorPositions.getChildren().add(penalty);
-
-
                 floor.getChildren().add(floorPositions);
-
             }
 
-
             playerBoardGrid.add(currentPlayerBoard, playerId.ordinal()%2, playerId.ordinal()/2);
-
         }
+        //Mise à jour du bord du pleteau du joueur courant
+        observer.map(ImmutableGameState::currentPlayerId).subscribe(currentId -> {
+            playerBoards.forEach((playerId, pane) -> {
+                if (playerId == currentId)
+                    pane.getStyleClass().add("current-player");
+                else
+                    pane.getStyleClass().remove("current-player");
+            });
+        });
 
 
 
